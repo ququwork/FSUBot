6480b20ee81e28fbb49b58f5e7459806b690a99c
Sean Pianka <pianka@eml.cc>
2017-02-01 00:32:59 -0500

    classifiers needs to be a list and not a tuple

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     keywords = ['florida', 'state', 'university', 'fsu', 'bot'],
     zip_safe=False,
     include_package_data=True,
-    classifiers=(
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Operating System :: Unix',
         'Operating System :: MacOS',
@@ -39,6 +39,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    ),
+    ],
 )
 
