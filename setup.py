3df8c138d3de5b2ecfa80a53818baec202aca359
Sean Pianka <pianka@eml.cc>
2017-01-31 23:53:24 -0500

    pip setup

---
+++ setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
+version = "0.1.0"
+
+setup(
+    name='fsubot',
+    version=version,
+    description='Base bot for developing FSU bots.',
+    long_description=readme,
+    author='Sean Pianka',
+    author_email='pianka@eml.cc',
+    url = 'https://github.com/seanpianka/fsubot',
+    download_url = 'https://github.com/seanpianka//tarball/{}'.format(version),
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        "selenium==3.0.2",
+        "lxml==3.7.2",
+        "argparse"
+    ],
+    license='MIT',
+    keywords = ['florida', 'state', 'university', 'fsu', 'bot'],
+    zip_safe=False,
+    include_package_data=True,
+    copyright = 'Copyright (c) 2017 Sean Pianka',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Education',
+        'Topic :: Utilities'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ),
+)
+
